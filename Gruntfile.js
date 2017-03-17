469799a7fe1a733e0f972b095fabc36b10380e79
prncher@gmail.com <prncher@gmail.com>
2017-03-17 14:14:19 -0700

    First commit of required files

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+		reporter: require('jshint-html-reporter'),
+        reporterOutput: 'jshint-report.html'
+      },
+      gruntfile: {
+        src: ['../app/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+};
